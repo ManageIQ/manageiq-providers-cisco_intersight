92761486e8fd707b5d4e1df25ec45b6a3139fa4b
TjazErzenSluzba <tjaz.erzen@xlab.si>
2021-12-03 08:36:45 +0100

    Implemented function raw_connect, included gem in file manager_mixin.rb

--- app/models/manageiq/providers/cisco_intersight/manager_mixin.rb
+++ app/models/manageiq/providers/cisco_intersight/manager_mixin.rb
@@ -107,16 +107,16 @@ module ManageIQ::Providers::CiscoIntersight::ManagerMixin
 
     def raw_connect(*args)
 
-      # TODO: When the intersight gem is refactored, change this function so that it returns client instead of connector
+      # TODO: To get this function working, change your `path_current` to where your tucson-v3 keys are
+      path_current = "/home/tjazerzen/intersight-client-keys"
 
-      path_current = "/home/tjazerzen/intersight-client-ruby"
       key_file_name = "/tucson-v3.key"
       keyid_file_name = "/tucson-v3.keyid"
       key_file_path = path_current + key_file_name
       keyid_file_path = path_current + keyid_file_name
       key = File.read(key_file_path).strip
       keyid = File.read(keyid_file_path).strip
-      connector = IntersightConnector.new(key, keyid)
+      connector = IntersightConnector.new(keyid, key)
       connector
 
     end
