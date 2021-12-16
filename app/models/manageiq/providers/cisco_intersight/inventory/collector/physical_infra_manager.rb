25d00c659249033122fc8fdc8579a70c5c620d00
tjazsch <tjaz.erzen@xlab.si>
2021-12-16 08:40:02 +0000

    integrated new gem in the collector and parser

--- app/models/manageiq/providers/cisco_intersight/inventory/collector/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/inventory/collector/physical_infra_manager.rb
@@ -2,15 +2,30 @@ module ManageIQ::Providers::CiscoIntersight
   class Inventory::Collector::PhysicalInfraManager < ManageIQ::Providers::Inventory::Collector
 
     def collect
+      
+      set_configuration
+
       physical_servers
     end
 
-    def connection
+    def set_configuration
       @connection ||= manager.connect
     end
 
+    def get_firmware_inventory_api
+      IntersightClient::FirmwareApi.new
+    end
+
+    def get_compute_api
+      IntersightClient::ComputeApi.new
+    end
+
+    def firmware_inventory
+      @firmware_inventory ||= get_firmware_inventory_api.get_firmware_running_firmware_list().results
+    end
+
     def physical_servers
-      @physical_servers ||= connection.get_physical_summaries_temporary.Results
+      @physical_servers ||= get_compute_api.get_compute_physical_summary_list.results
     end
 
   end
