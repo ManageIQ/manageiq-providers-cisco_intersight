6ffb391195c4884dc93e1252a2836c9b7edb0e7e
tjazsch <tjaz.erzen@xlab.si>
2021-12-17 10:20:06 +0000

    introduced lazy_find links between collections, built basic physical_servers_details parser and persister

--- app/models/manageiq/providers/cisco_intersight/inventory/persister/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/inventory/persister/physical_infra_manager.rb
@@ -6,7 +6,9 @@ module ManageIQ::Providers::CiscoIntersight
 
     def initialize_inventory_collections
       add_collection(physical_infra, :vms)
+      add_collection(physical_infra, :physical_server_firmwares)
       add_collection(physical_infra, :physical_servers)
+      add_collection(physical_infra, :physical_server_details)
     end
   end
 end
