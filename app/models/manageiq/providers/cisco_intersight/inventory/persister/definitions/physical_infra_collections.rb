a038a56ef0832f4a4e8043465c9a726271b80219
tjazsch <tjaz.erzen@xlab.si>
2021-12-17 11:07:19 +0000

    Revamped persister: Added & commented all the collections I'll need, now adding in 1 by 1

--- app/models/manageiq/providers/cisco_intersight/inventory/persister/definitions/physical_infra_collections.rb
+++ app/models/manageiq/providers/cisco_intersight/inventory/persister/definitions/physical_infra_collections.rb
@@ -2,8 +2,21 @@ module ManageIQ::Providers::CiscoIntersight::Inventory::Persister::Definitions::
   include ActiveSupport::Concern
 
   def initialize_physical_infra_collections
-    add_cloud_collection(:vms)
-    # TODO (Tjaz Erzen): Define physical infra collections.
+    %i(
+      physical_servers
+      physical_server_details
+    ).each do |name|
+      add_collection(physical_infra, name)
+    end    
+    # TODO: Add the following collections to the loop above after you start implementing new collections
+      # physical_server_computer_systems
+      # physical_server_hardwares
+      # physical_server_network_devices
+      # physical_server_storage_adapters
+      # physical_server_firmwares
 
+      # physical_racks
+      # physical_chassis
+      # physical_chassis_details
   end
 end
--- app/models/manageiq/providers/cisco_intersight/inventory/persister/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/inventory/persister/physical_infra_manager.rb
@@ -1,14 +1,10 @@
 module ManageIQ::Providers::CiscoIntersight
   class Inventory::Persister::PhysicalInfraManager < Inventory::Persister
 
-    include ActiveSupport::Concern
     include Inventory::Persister::Definitions::PhysicalInfraCollections
 
     def initialize_inventory_collections
-      add_collection(physical_infra, :vms)
-      add_collection(physical_infra, :physical_server_firmwares)
-      add_collection(physical_infra, :physical_servers)
-      add_collection(physical_infra, :physical_server_details)
+      initialize_physical_infra_collections
     end
   end
 end
