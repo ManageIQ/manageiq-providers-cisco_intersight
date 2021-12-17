5c5f782e63856205dcea88f35346fc580f754c93
tjazsch <tjaz.erzen@xlab.si>
2021-12-17 10:20:06 +0000

    introduced lazy_find links between collections, built basic physical_servers_details parser and persister

--- app/models/manageiq/providers/cisco_intersight/inventory/parser/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/inventory/parser/physical_infra_manager.rb
@@ -3,25 +3,68 @@ module ManageIQ::Providers::CiscoIntersight
 
     def parse
       physical_servers
+      physical_servers_details
+      # firmwares
     end 
 
     def physical_servers
       collector.physical_servers.each do |s|
+        
+        # Temporarily setting the values of rack and chassis to nil - as the other two collections are built,
+        # this is going to be changed by lazy_find function on the id
+        rack = nil
+        chassis = nil
 
         persister.physical_servers.build(
-          :ems_ref                => s.registered_device.moid,
-       	  :health_state           => "dummy",  
+          :ems_ref                => s.device_mo_id,
+       	  :health_state           => "unknown",  
        	  :hostname               => "dummy",  
        	  :name                   => s.name,
-       	  :physical_chassis       => "dummy",
-          :physical_rack          => "dummy",  
-       	  :power_state            => "dummy",
-          :raw_power_state        => "dummy",
+       	  :physical_chassis       => chassis,
+          :physical_rack          => rack,  
+       	  :power_state            => s.admin_power_state,
+          :raw_power_state        => s.oper_power_state,
           :type                   => "ManageIQ::Providers::CiscoIntersight::PhysicalInfraManager::PhysicalServer",
         )
       end
     end
 
+    def physical_servers_details
+      collector.physical_servers.each do |s|
+        server = persister.physical_servers.lazy_find(s.device_mo_id)
+        temp = "dummy"
+        
+
+       	persister.physical_server_details.build(
+       	  :description        => temp,
+      	  :location        => temp,
+      	  :location_led_state        => temp,
+      	  :machine_type        => temp,
+      	  :manufacturer        => temp,
+      	  :model        => s.model,
+      	  :product_name        => s.name,
+      	  :rack_name        => s.server_id,
+      	  :resource        => server,
+          :room        => temp,
+          :serial_number        => s.serial,
+        )
+      end
+    end
+       	
+       	
+
+
+
+    def firmwares
+      collector.firmware_inventory.each do |firmware|
+        persister.physical_server_firmwares.build(
+          :resource => nil,
+     	  :build => "dummy",
+     	  :name => "dummy",
+     	  :version => firmware.version
+        )
+      end
+    end
 
   end
 end
--- app/models/manageiq/providers/cisco_intersight/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/physical_infra_manager.rb
@@ -14,61 +14,7 @@ module ManageIQ::Providers::CiscoIntersight
     include ManagerMixin
     include_concern "Operations"
 
-    # TODO: This class represents a fake Ruby SDK with sample data.
-    #       Remove this and use a real Ruby SDK in the raw_connect method
-    class MyRubySDK
-      def vms
-        [
-          OpenStruct.new(
-            :id       => '1',
-            :name     => 'funky',
-            :location => 'dc-1',
-            :vendor   => 'unknown'
-          ),
-          OpenStruct.new(
-            :id       => '2',
-            :name     => 'bunch',
-            :location => 'dc-1',
-            :vendor   => 'unknown'
-          ),
-        ]
-      end
+    supports :create
 
-      def find_vm(id)
-        vms.find { |v| v.id == id.to_s }
-      end
-
-      def events
-        [
-          OpenStruct.new(
-            :name       => %w(instance_power_on instance_power_off).sample,
-            :id         => Time.zone.now.to_i,
-            :timestamp  => Time.zone.now,
-            :vm_ems_ref => [1, 2].sample
-          ),
-          OpenStruct.new(
-            :name       => %w(instance_power_on instance_power_off).sample,
-            :id         => Time.zone.now.to_i + 1,
-            :timestamp  => Time.zone.now,
-            :vm_ems_ref => [1, 2].sample
-          )
-        ]
-      end
-
-      def metrics(start_time, end_time)
-        timestamp = start_time
-        metrics = {}
-        while timestamp < end_time
-          metrics[timestamp] = {
-            'cpu_usage_rate_average'  => rand(100).to_f,
-            'disk_usage_rate_average' => rand(100).to_f,
-            'mem_usage_rate_average'  => rand(100).to_f,
-            'net_usage_rate_average'  => rand(100).to_f,
-          }
-          timestamp += 20.seconds
-        end
-        metrics
-      end
-    end
   end
 end
