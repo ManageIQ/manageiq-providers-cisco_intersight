bf641d34370fb9637676c1ab7d8bb90428ee2589
tjazsch <tjaz.erzen@xlab.si>
2021-12-20 10:46:49 +0000

    Collected physical racks and chassis

--- app/models/manageiq/providers/cisco_intersight/inventory/collector/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/inventory/collector/physical_infra_manager.rb
@@ -20,6 +20,18 @@ module ManageIQ::Providers::CiscoIntersight
       IntersightClient::ComputeApi.new
     end
 
+    def get_equipment_api
+      IntersightClient::EquipmentApi.new
+    end
+
+    def physical_racks
+      get_equipment_api.get_equipment_device_summary_list.results.select { |c| c.source_object_type == "compute.RackUnit" }
+    end
+
+    def physical_chassis
+      get_equipment_api.get_equipment_device_summary_list.results.reject { |c| c.source_object_type == "compute.RackUnit" }
+    end
+
     def firmware_inventory
       @firmware_inventory ||= get_firmware_inventory_api.get_firmware_running_firmware_list().results
     end
