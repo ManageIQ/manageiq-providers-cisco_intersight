d31200f8e8e322f57238f8d9bce4dd9e5abfe514
tjazsch <tjaz.erzen@xlab.si>
2021-12-23 16:34:20 +0000

    Wrote a bunch of TODOs in the parser, robustifyed the code

--- app/models/manageiq/providers/cisco_intersight/inventory/parser/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/inventory/parser/physical_infra_manager.rb
@@ -4,20 +4,17 @@ module ManageIQ::Providers::CiscoIntersight
     def parse
       physical_servers
       physical_servers_details
-      # firmwares
       physical_racks
       hardwares
-      # physical_server_network_devices
     end 
 
     def physical_servers
       collector.physical_servers.each do |s|
         
-        # Temporarily setting the values of rack and chassis to nil - as the other two collections are built,
-        # this is going to be changed by lazy_find function on the id
         rack = persister.physical_racks.lazy_find(s.device_mo_id)
+        # Since there is no data about the chassis on the Intersight side, I cannot obtain the data about the chassis. Setting its value to nil
         chassis = nil
-
+        # TODO: Obtain the data about health state, hostname
         server = persister.physical_servers.build(
           :ems_ref                => s.device_mo_id,
        	  :health_state           => "unknown",  
@@ -41,8 +38,7 @@ module ManageIQ::Providers::CiscoIntersight
       collector.physical_servers.each do |s|
         server = persister.physical_servers.lazy_find(s.device_mo_id)
         temp = "dummy"
-        
-
+        # TODO: Go through the data about the servers and obtain the data about the atributes, which at the moment hold value temp.
        	persister.physical_server_details.build(
        	  :description        => temp,
       	  :location        => temp,
@@ -59,17 +55,6 @@ module ManageIQ::Providers::CiscoIntersight
       end
     end
 
-    # def firmwares
-    #   collector.firmware_inventory.each do |firmware|
-    #     persister.physical_server_firmwares.build(
-    #       :resource => nil,
-    #  	  :build => "dummy",
-    #  	  :name => "dummy",
-    #  	  :version => firmware.version
-    #     )
-    #   end
-    # end
-
     def hardwares
       collector.physical_servers.each do |s|
         server = persister.physical_servers.lazy_find(s.device_mo_id)
@@ -77,69 +62,38 @@ module ManageIQ::Providers::CiscoIntersight
         hardware = persister.physical_server_hardwares.build(
           :computer_system => computer,
           :cpu_total_cores => s.num_cpus,
-          :disk_capacity   => s.total_memory,
+          :disk_capacity   => s.total_memory, # TODO: Replace this atribute, since it's not the right one.
           :memory_mb       => 0, # TODO: Reformat total memory and write it to mb && replace 0 with that value
           :cpu_speed       => s.cpu_capacity,
-       	  :disk_free_space => s.available_memory
+       	  :disk_free_space => s.available_memory # TODO: Replace this atribute, since it's not the right one.
         )
 
 	adapters_current = collector.physical_server_network_devices.select { |c| c.registered_device.moid == s.registered_device.moid }
-
         (adapters_current || []).each  do |net_adapter|
-
-          temp = net_adapter.registered_device.moid + "-" + net_adapter.dn
-
+          # TODO: Write atributes about the parent ID - set its value net_adapter.registered_device.moid
           persister.physical_server_network_devices.build(
        	    :hardware     => hardware,
-       	    :device_name  => net_adapter.dn, # temp
-       	    :device_type  => get_device_type3(net_adapter), # "ethernet", # temp, 
-       	    :manufacturer => "unknown", # question: is the first word in the net_adapter.model the manufacturer? 
-       	    :model        => net_adapter.model, # temp
-            :uid_ems	  => temp # net_adapter.registered_device.moid # temp
+       	    :device_name  => net_adapter.dn,
+       	    :device_type  => net_adapter.model,
+       	    :manufacturer => "unknown", # no data about the manufacturer - setting its value to unknown.
+       	    :model        => net_adapter.model,
+            :uid_ems	  => get_temporary_unique_identifyer(net_adapter.registered_device.moid, net_adapter.dn)
           )
         end
       end
     end
 
-    def get_device_type1(net_adapter)
-      adapter_model = net_adapter.model
-      if (adapter_model.include? "Ethernet") && (adapter_model.include? "Cisco")
-        device_type = "ethernet cisco"
-      elsif (adapter_model.include? "Controller") && (adapter_model.include? "Cisco")
-        device_type = "controller cisco"
-      elsif (adapter_model.include? "Ethernet") && (adapter_model.include? "Intel")
-        device_type = "ethernet intel"
-      elsif (adapter_model.include? "Controller") && (adapter_model.include? "Intel")
-        device_type = "controller intel"
-      else
-        device_type = "unknown"
-      end
-      device_type
-    end
-
-
-    def get_device_type2(net_adapter)
-      adapter_model = net_adapter.model
-      if (adapter_model.include? "Ethernet")
-        device_type = "ethernet"
-      elsif (adapter_model.include? "Controller")
-        device_type = "controller"
-      else
-	device_type = "unknown"
-      end
-      device_type
-    end
-
-    def get_device_type3(net_adapter)
-      device_type = "ethernet"                     
-      device_type
+    def get_temporary_unique_identifyer(moid, dn)
+      # Temporarily forming the uniquie identifyer from moid and dn - eventually, this will be replaced something else
+      # TODO: Ask the intersight people what the unique ID would look like.
+      moid + "-" + dn
     end
 
     def physical_racks
       collector.physical_racks.each do |r|
         persister.physical_racks.build(
           :ems_ref => r.registered_device.moid,
-          :name    => "dummy"
+          :name    => "dummy" # TODO: Obtain the name of the physical rack.
         )
       end
     end
