be1016778ddddd7e495750d962592fec76805495
TjazErzenSluzba <tjaz.erzen@xlab.si>
2022-02-10 14:15:47 +0100

    Reformatted dictionaries in parser.rb according to MiQ code standards

--- app/models/manageiq/providers/cisco_intersight/inventory/parser/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/inventory/parser/physical_infra_manager.rb
@@ -19,14 +19,14 @@ module ManageIQ::Providers::CiscoIntersight
         chassis = nil # TODO: After chassis gets written into the DB, obtain it and write it here as reference using lazy find.
         rack = nil # TODO: After chassis gets written into the DB, obtain it and write it here as reference using lazy find.
         server = persister.physical_servers.build(
-          :ems_ref => s.moid,
-          :health_state => get_health_state(s), # health_state obtained through atribute s.alarm_summary
-          :hostname => device_registration.device_hostname[0], # I assume one registered device manages one (and only one) compute element
-          :name => s.name,
+          :ems_ref          => s.moid,
+          :health_state     => get_health_state(s), # health_state obtained through atribute s.alarm_summary
+          :hostname         => device_registration.device_hostname[0], # I assume one registered device manages one (and only one) compute element
+          :name             => s.name,
           :physical_chassis => chassis, # nil for now
-          :physical_rack => rack, # nil for now
-          :power_state => s.oper_power_state,
-          :raw_power_state => s.admin_power_state,
+          :physical_rack    => rack, # nil for now
+          :power_state      => s.oper_power_state,
+          :raw_power_state  => s.admin_power_state,
           :type => "ManageIQ::Providers::CiscoIntersight::PhysicalInfraManager::PhysicalServer",
         )
         persister.physical_server_computer_systems.build(
@@ -42,15 +42,15 @@ module ManageIQ::Providers::CiscoIntersight
         source_object = collector.get_source_object_from_physical_server(s)
         device_contract_information_unit = collector.get_device_contract_information_from_device_moid(registered_device_moid)
         persister.physical_server_details.build(
-          :description => device_contract_information_unit.product.description,
-          :location => format_location(device_contract_information_unit),
+          :description        => device_contract_information_unit.product.description,
+          :location           => format_location(device_contract_information_unit),
           :location_led_state => get_locator_led_state(source_object),
-          :machine_type => device_contract_information_unit.device_type,
-          :model => s.model,
-          :product_name => s.name,
-          :resource => server,
-          :room => s.slot_id,
-          :serial_number => s.serial,
+          :machine_type       => device_contract_information_unit.device_type,
+          :model              => s.model,
+          :product_name       => s.name,
+          :resource           => server,
+          :room               => s.slot_id,
+          :serial_number      => s.serial,
         )
       end
     end
@@ -66,29 +66,29 @@ module ManageIQ::Providers::CiscoIntersight
         hardware = persister.physical_server_hardwares.build(
           :computer_system => computer,
           :cpu_total_cores => s.num_cpu_cores,
-          :memory_mb => s.available_memory,
-          :cpu_speed => s.cpu_capacity,
+          :memory_mb       => s.available_memory,
+          :cpu_speed       => s.cpu_capacity,
         )
 
         source_object.adapters.each do |adapter|
           adapter_unit = collector.get_adapter_unit_by_moid(adapter.moid)
           management_controller_unit = collector.get_management_controller_by_moid(adapter_unit.controller.moid)
           persister.physical_server_network_devices.build(
-            :hardware => hardware,
-            :device_name => s.name,
-            :device_type => "ethernet",
+            :hardware     => hardware,
+            :device_name  => s.name,
+            :device_type  => "ethernet",
             :manufacturer => get_manufacturer_from_management_controller(management_controller_unit),
-            :model => management_controller_unit.model,
-            :uid_ems => adapter_unit.moid
+            :model        => management_controller_unit.model,
+            :uid_ems      => adapter_unit.moid
           )
         end
 
         storage_controllers_list.each do |storage_controller_reference|
           persister.physical_server_storage_adapters.build(
-            :hardware => hardware,
+            :hardware    => hardware,
             :device_name => s.name,
             :device_type => "storage",
-            :uid_ems => storage_controller_reference.moid
+            :uid_ems     => storage_controller_reference.moid
           )
         end
       end
@@ -96,10 +96,10 @@ module ManageIQ::Providers::CiscoIntersight
 
     def physical_racks
       collector.physical_racks.each do |r|
+        # No data about the physical_racks yet, so I cannot parse the data about it.
         persister.physical_racks.build(
           :ems_ref => r.registered_device.moid,
-          # TODO: r.dn instead of the "dummy"
-          :name => "to-be-done"
+          :name    => ""
         )
       end
     end
@@ -112,9 +112,9 @@ module ManageIQ::Providers::CiscoIntersight
           hardware = persister.physical_server_hardwares.lazy_find(computer)
           persister.physical_server_firmwares.build(
             :resource => hardware,
-            :build => component_fw_inventory.label,
-            :name => component_fw_inventory.label,
-            :version => component_fw_inventory.version
+            :build    => component_fw_inventory.label,
+            :name     => component_fw_inventory.label,
+            :version  => component_fw_inventory.version
           )
         end
       end
@@ -123,9 +123,9 @@ module ManageIQ::Providers::CiscoIntersight
     def physical_chassis
       collector.physical_chassis.each do |c|
         persister.physical_chassis.build(
-          :ems_ref => c.moid,
+          :ems_ref      => c.moid,
           :health_state => get_health_state(c),
-          :name => c.name
+          :name         => c.name
         )
       end
     end
@@ -136,13 +136,13 @@ module ManageIQ::Providers::CiscoIntersight
         device_contract_information_unit = collector.get_device_contract_information_from_device_moid(registered_device_moid)
         chassis = persister.physical_chassis.lazy_find(c.moid)
         persister.physical_chassis_details.build(
-          :description => device_contract_information_unit.product.description,
-          :location => format_location(device_contract_information_unit),
+          :description        => device_contract_information_unit.product.description,
+          :location           => format_location(device_contract_information_unit),
           :location_led_state => get_locator_led_state(c),
-          :model => c.model,
-          :part_number => c.part_number,
-          :resource => chassis,
-          :serial_number => c.serial
+          :model              => c.model,
+          :part_number        => c.part_number,
+          :resource           => chassis,
+          :serial_number      => c.serial
         )
       end
     end
