f16cd8461d8925f8ce4b37fbd00215509db69478
Adam Grare <adam@grare.com>
2022-05-13 12:39:10 -0400

    Revert "Merge pull request #60 from jrafanie/fix_display_name"
    This reverts commit c0e261550db243ac9ab2825e01f781f4fec1d059, reversing
    changes made to 948ac7a302dae3511d17c4adf0cfef734a996a77.

--- app/models/manageiq/providers/cisco_intersight/physical_infra_manager/physical_server_profile.rb
+++ app/models/manageiq/providers/cisco_intersight/physical_infra_manager/physical_server_profile.rb
@@ -1,7 +1,7 @@
 module ManageIQ::Providers::CiscoIntersight
   class PhysicalInfraManager::PhysicalServerProfile < ::PhysicalServerProfile
-    def self.display_name(number = 1)
-      n_('Physical Server Profile (CiscoIntersight)', 'Physical Server Profile (CiscoIntersight)', number)
+    def self.display_name(name)
+      n_('Physical Server Profile (CiscoIntersight)', 'Physical Server Profile (CiscoIntersight)', name)
     end
 
     def provider_object(connection)
