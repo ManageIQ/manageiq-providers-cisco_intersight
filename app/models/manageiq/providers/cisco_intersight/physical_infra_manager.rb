b581d6086e80a07f9a01aca26e8929cabf637425
eilam <eilam.elimelech@kyndryl.com>
2022-11-18 11:54:19 +0200

    add physical server profile template

--- app/models/manageiq/providers/cisco_intersight/physical_infra_manager.rb
+++ app/models/manageiq/providers/cisco_intersight/physical_infra_manager.rb
@@ -1,6 +1,5 @@
 module ManageIQ::Providers::CiscoIntersight
   class PhysicalInfraManager < ManageIQ::Providers::PhysicalInfraManager
-    supports :PhysicalServerProfileTemplate
     require_nested :MetricsCapture
     require_nested :MetricsCollectorWorker
     require_nested :Refresher
