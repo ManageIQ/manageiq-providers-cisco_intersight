aa04aa6ab2ca06364bdf18e1f21f0344adb4b1f4
eilam <eilam.elimelech@kyndryl.com>
2022-11-24 11:09:18 +0200

    call connect functions with the ems move deploy_server_from_template_queue to core set the deploy_server_from_template function to be an instance method

--- app/models/manageiq/providers/cisco_intersight/physical_infra_manager/physical_server_profile_template.rb
+++ app/models/manageiq/providers/cisco_intersight/physical_infra_manager/physical_server_profile_template.rb
@@ -8,29 +8,13 @@ module ManageIQ::Providers::CiscoIntersight
       connection.find!(ems_ref)
     end
 
-    def deploy_server_from_template_queue(server_id, profile_name)
-      task_opts = {
-        :action => "Deploy server from profile template",
-      }
 
-      queue_opts = {
-        :class_name  => self.class.name,
-        :method_name => 'deploy_server_from_template',
-        :role        => 'ems_operations',
-        :queue_name  => ext_management_system.queue_name_for_ems_operations,
-        :zone        => ext_management_system.my_zone,
-        :args        => [ems_ref, server_id, profile_name, ext_management_system.id]
-      }
-
-      MiqTask.generic_action_with_callback(task_opts, queue_opts)
-    end
-
-    def self.deploy_server_from_template(template_id, server_id, profile_name, ext_management_system_id)
+    def deploy_server_from_template(server_id, profile_name)
       # Load the gem
       require 'intersight_client'
 
-      bulk = ManageIQ::Providers::CiscoIntersight::PhysicalInfraManager.first.connect(:service=>'BulkApi')
-      cloner = IntersightClient::BulkMoCloner.new({:sources => [{"Moid" => template_id, "ObjectType" => 'server.ProfileTemplate'}], :targets => [{"Name" => profile_name, :ObjectType => 'server.Profile'}]})
+      bulk = ext_management_system.connect(:service=>'BulkApi')
+      cloner = IntersightClient::BulkMoCloner.new({:sources => [{"Moid" => ems_ref, "ObjectType" => 'server.ProfileTemplate'}], :targets => [{"Name" => profile_name, :ObjectType => 'server.Profile'}]})
 
       # create a new server profile from server profile template
 
@@ -46,7 +30,7 @@ module ManageIQ::Providers::CiscoIntersight
         }
       )
       # assign the profile server to the selected server
-      server_api = ManageIQ::Providers::CiscoIntersight::PhysicalInfraManager.first.connect(:service=>'ServerApi')
+      server_api = ext_management_system.connect(:service=>'ServerApi')
       begin
         result = server_api.patch_server_profile(new_profile_moid, server_profile_updated, {})
         _log.info("Server profile successfully assigned with server #{server_id} (ems_ref #{result.assigned_server.moid})")
@@ -67,9 +51,9 @@ module ManageIQ::Providers::CiscoIntersight
 
       # create a job to refresh the cisco intersight provider after the deployment finished
       options = {
-        :target_class   => "ManageIQ::Providers::CiscoIntersight::PhysicalInfraManager::PhysicalServerProfileTemplate",
-        :target_id      => nil,
-        :ems_id         => ext_management_system_id,
+        :target_class   =>self.class.name,
+        :target_id      => id,
+        :ems_id         => ext_management_system.id,
         :native_task_id => new_profile_moid,
         :interval       => 30.seconds,
         :target_option  => "deploy"
